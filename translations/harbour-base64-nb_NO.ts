8a9312e532b277c9d50474c5d13afc852f6560d0
Allan Nordhøy <epost@anotheragency.no>
2017-12-21 09:50:06 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/harbour-base64-nb_NO.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="41"/>
+        <source>Copy to clipboard</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Clear</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="56"/>
+        <source>Base64 to Text</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="69"/>
+        <source>Text to Base64</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="75"/>
+        <source>Any text…</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="83"/>
+        <location filename="../qml/pages/MainPage.qml" line="125"/>
+        <source>Encode</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="127"/>
+        <source>Decode</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
