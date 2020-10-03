d6ea4ad87f5cc67f56c3200f89fa54a902ab2274
Yaron Shahrabani <sh.yaron@gmail.com>
2020-10-03 22:25:17 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Harbour Base64/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-base64/translations/he/

--- translations/harbour-base64-he.ts
+++ translations/harbour-base64-he.ts
@@ -6,38 +6,38 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="41"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>העתקה ללוח הגזירים</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="49"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>פינוי</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="56"/>
         <source>Base64 to Text</source>
-        <translation type="unfinished"></translation>
+        <translation>בסיס64 לטקסט</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="69"/>
         <source>Text to Base64</source>
-        <translation type="unfinished"></translation>
+        <translation>טקסט לבסיס64</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <source>Any text…</source>
-        <translation type="unfinished"></translation>
+        <translation>טקסט כלשהו…</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="83"/>
         <location filename="../qml/pages/MainPage.qml" line="125"/>
         <source>Encode</source>
-        <translation type="unfinished"></translation>
+        <translation>הצפנה</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="127"/>
         <source>Decode</source>
-        <translation type="unfinished"></translation>
+        <translation>פענוח</translation>
     </message>
 </context>
 </TS>
