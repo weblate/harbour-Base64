79c2248f5b5657713224c0ad8faceff734b7cedc
phlostically <phlostically@mailinator.com>
2021-03-11 23:11:39 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Harbour Base64/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-base64/translations/eo/

--- translations/harbour-base64-eo.ts
+++ translations/harbour-base64-eo.ts
@@ -6,38 +6,38 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="41"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopii en tondujon</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="49"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Forviŝi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="56"/>
         <source>Base64 to Text</source>
-        <translation type="unfinished"></translation>
+        <translation>64-umaĵo al Teksto</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="69"/>
         <source>Text to Base64</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksto al 64-umaĵo</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <source>Any text…</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajna teksto…</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="83"/>
         <location filename="../qml/pages/MainPage.qml" line="125"/>
         <source>Encode</source>
-        <translation type="unfinished"></translation>
+        <translation>Kodi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="127"/>
         <source>Decode</source>
-        <translation type="unfinished"></translation>
+        <translation>Malkodi</translation>
     </message>
 </context>
 </TS>
