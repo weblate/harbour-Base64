11a798f583f7bc42b306fb3a0acbce9e77a27204
aberwag <aberwag@gmail.com>
2022-03-21 13:11:51 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Harbour Base64/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-base64/translations/ar/

--- translations/harbour-base64-ar.ts
+++ translations/harbour-base64-ar.ts
@@ -6,38 +6,38 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="41"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>نسخ إلى الحافظة</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="49"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>مسح</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="56"/>
         <source>Base64 to Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Base64 إلى نص</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="69"/>
         <source>Text to Base64</source>
-        <translation type="unfinished"></translation>
+        <translation>نص إلى Base64</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <source>Any text…</source>
-        <translation type="unfinished"></translation>
+        <translation>أي نص…</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="83"/>
         <location filename="../qml/pages/MainPage.qml" line="125"/>
         <source>Encode</source>
-        <translation type="unfinished"></translation>
+        <translation>تشفير</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="127"/>
         <source>Decode</source>
-        <translation type="unfinished"></translation>
+        <translation>فك تشفير</translation>
     </message>
 </context>
 </TS>
